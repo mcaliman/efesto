8a42a4305255c7f1d5f331a29907349273eb34f8
mcaliman <mcaliman@gmail.com>
2018-11-17 21:26:53 +0100

    refactoring

--- src/excel/grammar/formula/reference/RangeReference.java
+++ src/excel/grammar/formula/reference/RangeReference.java
@@ -93,11 +93,17 @@ public final class RangeReference extends Reference {
     @Override
     public String toString(boolean address) {
         StringBuilder buff = new StringBuilder();
-        if (address && !sameAddr(reference1) && getColumn() != -1 && getRow() != -1) buff.append(getAddress()).append(" = ");
-        if (sheetName != null && sheetName.trim().length() > 0) buff.append(sheetName).append("!");
-        buff.append(reference1.getAddress()).append(":").append(reference2.getAddress());
-        if (address) buff.append(" = ").append(values());
-        return buff.toString();
+        if(address){
+            if (!sameAddr(reference1) && getColumn() != -1 && getRow() != -1) buff.append(getAddress()).append(" = ");
+            if (sheetName != null && sheetName.trim().length() > 0) buff.append(sheetName).append("!");
+            buff.append(reference1.getAddress()).append(":").append(reference2.getAddress());
+            buff.append(" = ").append(values());
+            return buff.toString();
+        }else{
+            if (sheetName != null && sheetName.trim().length() > 0) buff.append(sheetName).append("!");
+            buff.append(reference1.getAddress()).append(":").append(reference2.getAddress());
+            return buff.toString();
+        }
     }
 
 }
