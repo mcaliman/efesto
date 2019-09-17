dc97d8edceed7f66274233eadafed576fb567fb3
mcaliman <mcaliman@gmail.com>
2019-09-17 08:23:45 +0200

    refactoring

---
+++ test/UNION.vb
@@ -0,0 +1,14 @@
+'' 
+'' Text File: test/UNION.vb
+'' Excel File: UNION.xlsx
+'' Excel Formulas Number: 1
+'' Elapsed Time (parsing + topological sort): 0 s. or 0 min.
+'' creator:null
+'' description:null
+'' keywords:null
+'' title:null
+'' subject:null
+'' category:null
+A1:B2 = [[1.0 2.0][3.0 5.0]]
+C2:C3 = [ 6.0 8.0 ]
+A5 = SUM(A1:B2,C2:C3)
