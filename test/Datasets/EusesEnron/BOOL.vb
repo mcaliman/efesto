643e996141186ef00a6d70a433fffd3aacc2c99f
mcaliman <mcaliman@gmail.com>
2019-09-19 07:58:36 +0200

    refactoring

---
+++ test/Datasets/EusesEnron/BOOL.vb
@@ -0,0 +1,16 @@
+'' 
+'' Text File: test/Datasets/EusesEnron/BOOL.vb
+'' Excel File: BOOL.xlsx
+'' Excel Formulas Number: 2
+'' Elapsed Time (parsing + topological sort): 0 s. or 0 min.
+'' creator:null
+'' description:null
+'' keywords:null
+'' title:null
+'' subject:null
+'' category:null
+A3 = 1.0
+A4 = TRUE
+A5 = "IFTRUE"
+A6 = "IFFALSE"
+A1 = IF(AND(A3=1,A4=TRUE),A5,A6)
