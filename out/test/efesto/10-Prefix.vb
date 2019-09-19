0382900cd71ff08e1d1d08fa7609fb5ba02f10aa
mcaliman <mcaliman@gmail.com>
2019-09-19 08:39:58 +0200

    refactoring

---
+++ out/test/efesto/10-Prefix.vb
@@ -0,0 +1,13 @@
+'' 
+'' Text File: test/10-Prefix.vb
+'' Excel File: 10-Prefix.xlsx
+'' Excel Formulas Number: 1
+'' Elapsed Time (parsing + topological sort): 0 s. or 0 min.
+'' creator:null
+'' description:null
+'' keywords:null
+'' title:null
+'' subject:null
+'' category:null
+Sheet1!A1 = 78.0
+Prefix!A1 = Sheet1!A1
---
+++ out/test/efesto/metadata.vb
@@ -0,0 +1,13 @@
+'' 
+'' Text File: test/metadata.vb
+'' Excel File: metadata.xlsx
+'' Excel Formulas Number: 1
+'' Elapsed Time (parsing + topological sort): 0 s. or 0 min.
+'' creator:null
+'' description:Metatadato Commenti
+'' keywords:Metadati Tag1 Tag2
+'' title:Metatdato Titolo
+'' subject:Metadata Oggetto
+'' category:Metadato Categoria
+A1 = 12.0
+A2 = A1+1
