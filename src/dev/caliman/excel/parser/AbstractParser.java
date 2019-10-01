c282d70d1cefb1eb1c98ef3708ef58571949118c
mcaliman <mcaliman@gmail.com>
2019-10-01 09:00:59 +0200

    refactoring

---
+++ src/dev/caliman/excel/parser/AbstractParser.java
@@ -0,0 +1,57 @@
+/*
+ * Efesto - Excel Formula Extractor System and Topological Ordering algorithm.
+ * Copyright (C) 2017 Massimo Caliman mcaliman@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * If AGPL Version 3.0 terms are incompatible with your use of
+ * Efesto, alternative license terms are available from Massimo Caliman
+ * please direct inquiries about Efesto licensing to mcaliman@gmail.com
+ */
+
+package dev.caliman.excel.parser;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public abstract class AbstractParser {
+
+    protected Workbook xlsxBook;
+    protected Sheet xlsxSheet;//(Work)Sheet
+
+    protected int getSheetIndex() {
+        return this.xlsxBook.getSheetIndex(this.xlsxSheet);
+    }
+
+    protected String getSheetName() {
+        return this.xlsxSheet.getSheetName();
+    }
+
+
+    protected String getSheetName(Cell xlsxCell) {
+        return xlsxCell.getSheet().getSheetName();
+    }
+
+    protected boolean empty(final Cell xlsxCell) {
+        if(xlsxCell==null) { // use row.getCell(x, Row.CREATE_NULL_AS_BLANK) to avoid null cells
+            return true;
+        }
+        if(xlsxCell.getCellType()==Cell.CELL_TYPE_BLANK) {
+            return true;
+        }
+        return xlsxCell.getCellType()==Cell.CELL_TYPE_STRING&&xlsxCell.getStringCellValue().trim().isEmpty();
+    }
+
+}
